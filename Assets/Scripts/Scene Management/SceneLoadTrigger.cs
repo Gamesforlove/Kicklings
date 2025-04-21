49dc774635048831aacb59194e21ff0842792a37
sergi-borras <109757727+Sergi961010@users.noreply.github.com>
2025-04-21 11:38:17 +0200

    Fixed error after moving the repository to GFL domain

--- Assets/Scripts/Scene Management/SceneLoadTrigger.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class SceneLoadTrigger : MonoBehaviour
-{
-    [SerializeField] private Scene scene;
-    public void LoadScene() => SceneHandler.Instance.LoadScene(scene);
-}
