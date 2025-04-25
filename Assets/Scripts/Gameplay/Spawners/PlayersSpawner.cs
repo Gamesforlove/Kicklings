86bf37a94ae3208ef8033c867a66e0d59403cdec
sergi-borras <109757727+Sergi961010@users.noreply.github.com>
2025-04-25 13:08:02 +0200

    Fixed right side players behaviour on 4p mode

--- Assets/Scripts/Gameplay/Spawners/PlayersSpawner.cs
+++ Assets/Scripts/Gameplay/Spawners/PlayersSpawner.cs
@@ -8,14 +8,7 @@ public class PlayersSpawner : MonoBehaviour
     public GameObject SpawnCpu(Transform spawnPosition)
     {
         GameObject go = Instantiate(_cpuPrefab, spawnPosition.position, spawnPosition.rotation);
-        go.transform.localScale = new Vector3(-1, 1, 1);
-        HingeJoint2D joint = go.transform.Find("Visuals/Legs/RightLeg").gameObject.GetComponent<HingeJoint2D>();
-        JointAngleLimits2D limits = new()
-        {
-            min = 90f,
-        };
-        joint.limits = limits;
-        go.GetComponent<PlayerActions>().KickingLegSpeed *= -1;
+        ModifyComponentsForRightSide(go);
         return go;
     }
 
@@ -24,7 +17,19 @@ public class PlayersSpawner : MonoBehaviour
         //GameObject go = Instantiate(_playerPrefab, spawnPosition.position, spawnPosition.rotation);
         GameObject go = PlayerInput.Instantiate(_playerPrefab, controlScheme: scheme.name, pairWithDevice: Keyboard.current).gameObject;
         go.transform.position = spawnPosition.position;
-        if (spawnPosition.position.x > 0) go.transform.localScale = new Vector3(-1, 1, 1);
+        if (spawnPosition.position.x > 0) ModifyComponentsForRightSide(go);
         return go;
+    }
+
+    void ModifyComponentsForRightSide(GameObject go)
+    {
+        go.transform.localScale = new Vector3(-1, 1, 1);
+        HingeJoint2D joint = go.transform.Find("Visuals/Legs/RightLeg").gameObject.GetComponent<HingeJoint2D>();
+        JointAngleLimits2D limits = new()
+        {
+            min = 90f,
+        };
+        joint.limits = limits;
+        go.GetComponent<PlayerActions>().KickingLegSpeed *= -1;
     }
 }
