2e1d03900345cc04701aa9c67ff30471bb8a8ebb
sergi-borras <109757727+Sergi961010@users.noreply.github.com>
2025-04-26 14:46:23 +0200

    Added CustomBuild.cs to be used by github actions workflow, it disables compression for webgl builds in order to deploy to github pages

---
+++ Assets/Editor/CustomBuild.cs
@@ -0,0 +1,35 @@
+﻿using UnityEditor;
+using UnityEngine;
+using System.Linq;
+
+namespace Editor
+{
+    public class CustomBuild
+    {
+        public static void Build()
+        {
+            if (EditorUserBuildSettings.activeBuildTarget == BuildTarget.WebGL)
+            {
+                Debug.Log("Setting WebGL compression to Disabled...");
+                PlayerSettings.WebGL.compressionFormat = WebGLCompressionFormat.Disabled;
+                PlayerSettings.WebGL.decompressionFallback = false;
+            }
+
+            // Automatically get all enabled scenes
+            string[] scenes = EditorBuildSettings.scenes
+                .Where(scene => scene.enabled)
+                .Select(scene => scene.path)
+                .ToArray();
+
+            BuildPlayerOptions buildPlayerOptions = new()
+            {
+                scenes = scenes,
+                locationPathName = "build/" + EditorUserBuildSettings.activeBuildTarget,
+                target = EditorUserBuildSettings.activeBuildTarget,
+                options = BuildOptions.None
+            };
+
+            BuildPipeline.BuildPlayer(buildPlayerOptions);
+        }
+    }
+}
