f708aebcb3b6abf81ce6296426c231401c06bf46
sergi-borras <109757727+Sergi961010@users.noreply.github.com>
2025-04-30 15:12:10 +0200

    Added character customization visuals and logic on main menu for free mode

---
+++ Assets/Scripts/UI/MainMenu/FreeMode/CharacterCustomizationController.cs
@@ -0,0 +1,59 @@
+using CommonDataTypes;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace UI.MainMenu.FreeMode
+{
+    public class CharacterCustomizationController : MonoBehaviour
+    {
+        [SerializeField] CharacterCustomizationImages _customizationImages;
+        [SerializeField] FieldSideType  _fieldSideType;
+        [SerializeField] Image _shirtImage,  _shoesLeftImage, _shoesRightImage;
+        int _shirtIndex, _shoesIndex;
+        string _shirtKey, _shoesKey;
+
+        void Awake()
+        {
+            _shirtKey = _fieldSideType + "ShirtImageIndex";
+            _shoesKey = _fieldSideType + "ShoesImageIndex";
+        }
+
+        void Start()
+        {
+            _shirtIndex = PlayerPrefs.GetInt(_shirtKey, 0);
+            _shoesIndex = PlayerPrefs.GetInt(_shoesKey, 0);
+            _shirtImage.sprite = _customizationImages.GetShirtSprite(_shirtIndex);
+            _shoesLeftImage.sprite = _customizationImages.GetShoesSprite(_shoesIndex);
+            _shoesRightImage.sprite = _customizationImages.GetShoesSprite(_shoesIndex);
+        }
+        
+        public void ChangeShirt(int nextIndex)
+        {
+            int newIndex = GetNextShirtIndex(nextIndex);
+            _shirtImage.sprite = _customizationImages.GetShirtSprite(newIndex);
+            PlayerPrefs.SetInt(_shirtKey, newIndex);
+            _shirtIndex = newIndex;
+        }
+
+        public void ChangeShoes(int nextIndex)
+        {
+            int newIndex = GetNextShoesIndex(nextIndex);
+            _shoesLeftImage.sprite = _customizationImages.GetShoesSprite(newIndex);
+            _shoesRightImage.sprite = _customizationImages.GetShoesSprite(newIndex);
+            PlayerPrefs.SetInt(_shoesKey, newIndex);
+            _shoesIndex = newIndex;
+        }
+
+        int GetNextShirtIndex(int delta)
+        {
+            int count = _customizationImages.GetShirtSpriteCount();
+            return (_shirtIndex + delta + count) % count;
+        }
+        
+        int GetNextShoesIndex(int delta)
+        {
+            int count = _customizationImages.GetShoesSpriteCount();
+            return (_shirtIndex + delta + count) % count;
+        }
+    }
+}
---
+++ Assets/Scripts/UI/MainMenu/FreeMode/CharacterCustomizationImages.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace UI.MainMenu.FreeMode
+{
+    [CreateAssetMenu(fileName = "CharacterCustomizationImages", menuName = "UI/FreeMode/CharacterCustomizationImages")]
+    public class CharacterCustomizationImages : ScriptableObject
+    {
+        [SerializeField] List<Sprite> _shirtImages = new();
+        [SerializeField] List<Sprite> _shoesImages = new();
+
+        public Sprite GetShirtSprite(int index) => _shirtImages[index];
+        public Sprite GetShoesSprite(int index) => _shoesImages[index];
+
+        public int GetShirtSpriteCount() => _shirtImages.Count;
+        public int GetShoesSpriteCount() => _shoesImages.Count;
+    }
+}
