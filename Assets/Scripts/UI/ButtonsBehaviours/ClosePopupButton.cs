bbf2c78ac3b2f25c8e203ba18c285350d9bf6daf
sergi-borras <109757727+Sergi961010@users.noreply.github.com>
2025-06-02 20:27:45 +0200

    Unified main menu popup aesthetics

---
+++ Assets/Scripts/UI/ButtonsBehaviours/ClosePopupButton.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using UI.UiSystem.Core;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace UI.ButtonsBehaviours
+{
+    [RequireComponent(typeof(Button))]
+    public class ClosePopupButton : MonoBehaviour
+    {
+        UIViewsManager _uiViewsManager;
+        UIView _view;
+        Button _button;
+
+        void Awake()
+        {
+            _uiViewsManager = FindFirstObjectByType<UIViewsManager>();
+            _view = GetComponentInParent<UIView>();
+            _button = GetComponent<Button>();
+        }
+
+        void Start()
+        {
+            _button.onClick.AddListener(() => _uiViewsManager.HideView(_view));
+        }
+    }
+}
