a56f20dd395e7fcf7f19cc627358f46ef4e104ea
Rishi Amarnani <rsaamar@gmail.com>
2026-01-26 02:53:34 -0600

    Created the stage flow scene tooling, and automated build settings editing and added test scenes as an example

--- Assets/Scripts/Campaign/CampaignTools/StageFlow.cs
+++ Assets/Scripts/Campaign/CampaignTools/StageFlow.cs
@@ -1,22 +1,53 @@
 using UnityEngine;
 using System.Collections.Generic;
 
+#if UNITY_EDITOR
+    using UnityEditor;
+    using System.Linq;
+#endif
+
 [CreateAssetMenu(fileName = "StageFlow", menuName = "Scriptable Objects/StageFlow")]
-[System.Serializable]
 public class StageFlow : ScriptableObject
 {
-    public List<SceneInfo> scenes;
+    public List<SceneInfo> scenes = new();
+
+#if UNITY_EDITOR
+    private void OnValidate()
+    {
+        if (scenes == null) return;
+
+        foreach (SceneInfo info in scenes)
+        {
+            if (info == null || info.sceneAsset == null) continue;
+
+            string path = AssetDatabase.GetAssetPath(info.sceneAsset);
+            info.SetPathEditorOnly(path);
+            bool inBuild = EditorBuildSettings.scenes
+                .Any(s => s != null && s.enabled && s.path == path);
+
+            if (!inBuild)
+                Debug.LogWarning($"Scene '{info.sceneAsset.name}' is not ENABLED in Build Settings. " +
+                    $"Use the button below to add it.", this);
+        }
+    }
+#endif
 }
 
 [System.Serializable]
 public class SceneInfo
 {
-    public string sceneName;
+#if UNITY_EDITOR
+    public SceneAsset sceneAsset;
+    public void SetPathEditorOnly(string path) => scenePath = path;
+#endif
+
     public SceneType sceneType;
-    public string Description;
+    [TextArea] public string description;
+
+    [SerializeField] private string scenePath;
+    public string Path => scenePath;
 }
 
-[System.Serializable]
 public enum SceneType
 {
     Gameplay,
