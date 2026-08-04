98497aa231b27b28bb961a762cbc66bc3107280c
Rishi Amarnani <rsaamar@gmail.com>
2026-08-04 18:12:27 -0500

    Added stage and scene vars to StorageData for campaign progress tracking

--- Assets/Scripts/SaveSystem/StorageData.cs
+++ Assets/Scripts/SaveSystem/StorageData.cs
@@ -12,6 +12,10 @@ namespace SaveSystem
             abilities = new Dictionary<int, string>();
         }
 
+        // campaign progress tracking
+        public int stage;
+        public int scene;
+
         public int intData;
         public string stringData;
         public Dictionary<int, string> abilities;
